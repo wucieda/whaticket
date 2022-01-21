d8002c06fb7c99270d3a58cc9d3adf5128c46923
wucieda <wilder.ucieda@gmail.com>
2022-01-21 11:07:01 -0400

    restart connections

---
+++ frontend/build/precache-manifest.44d4ac890537352e6e739c300d16fd2c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7554d3ab29659c76623da9d36ad8d0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9747c3cb66938b334f87",
+    "url": "/static/css/2.d180f738.chunk.css"
+  },
+  {
+    "revision": "9747c3cb66938b334f87",
+    "url": "/static/js/2.86644b97.chunk.js"
+  },
+  {
+    "revision": "93a1d1093f425a86f590236b51f321bc",
+    "url": "/static/js/2.86644b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3958d8bd08fa479be6",
+    "url": "/static/js/3.ad0a2f01.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.ad0a2f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bcd662207d0491b804",
+    "url": "/static/js/main.733f1e15.chunk.js"
+  },
+  {
+    "revision": "cb30e63b4a7a0f378fc2",
+    "url": "/static/js/runtime-main.c80d7624.js"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0f59014c1e1fc110381a3a231888f54.js"
+  "/precache-manifest.44d4ac890537352e6e739c300d16fd2c.js"
 );
 
 self.addEventListener('message', (event) => {
